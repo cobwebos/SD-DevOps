b36c8591f7bb0e8acbe7da90af5087806b3b301b
Nicolas De Loof <nicolas.deloof@gmail.com>
2013-07-05 11:14:03 +0200

    compress JS and generate unique name for static ressources

--- war/Gruntfile.js
+++ war/Gruntfile.js
@@ -1,9 +1,33 @@
 module.exports = function (grunt) {
-  grunt.initConfig({
 
+  // load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+    uglify: {
+      dist: {
+        files: {
+          'target/jenkins/scripts/jenkins.js': [
+            'src/main/webapp/scripts/*.js'
+          ]
+        }
+      }
+    },
+    rev: {
+      dist: {
+        files: {
+          src: [
+            'target/jenkins/scripts/jenkins.js',
+            'target/jenkins/scripts/yui/{,**/}*.js'
+          ]
+        }
+      }
+    }
   });
 
   grunt.registerTask('build', [
+      'uglify',
+      'rev'
   ]);
 
   grunt.registerTask('default', ['build']);
